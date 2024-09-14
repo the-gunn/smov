8070a8b11ac2f5d37d7ebbe604d08326660a8123
the-gunn <79159538+the-gunn@users.noreply.github.com>
2024-09-14 15:30:54 +0100

    Update Navigation.tsx

--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -94,22 +94,6 @@ export function Navigation(props: NavigationProps) {
                 <BrandPill clickable header />
               </Link>
               <a
-                href={conf().DISCORD_LINK}
-                target="_blank"
-                rel="noreferrer"
-                className="text-xl text-white tabbable rounded-full"
-              >
-                <IconPatch icon={Icons.DISCORD} clickable downsized />
-              </a>
-              <a
-                href={conf().GITHUB_LINK}
-                target="_blank"
-                rel="noreferrer"
-                className="text-xl text-white tabbable rounded-full"
-              >
-                <IconPatch icon={Icons.GITHUB} clickable downsized />
-              </a>
-              <a
                 onClick={() => handleClick("/discover")}
                 rel="noreferrer"
                 className="text-xl text-white tabbable rounded-full"
